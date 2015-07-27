d00ea4e4ae858ea5c4947365135861617446f84e
Sergei Maertens <sergei@maykinmedia.nl>
2015-07-27 17:47:53 +0200

    Proper admin styling for custom form, show form errors

--- tests/app/tests/test_views.py
+++ tests/app/tests/test_views.py
@@ -4,7 +4,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django.test import TestCase
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ugettext as _
 
 try:
     from django.test import override_settings
