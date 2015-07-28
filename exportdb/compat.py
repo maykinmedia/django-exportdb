83c677ef49d040eb14e4c3db952cc6736e3adfbb
Sergei Maertens <sergei@maykinmedia.nl>
2015-07-28 12:31:00 +0200

    added noqa comment

--- exportdb/compat.py
+++ exportdb/compat.py
@@ -1,4 +1,4 @@
 try:
     from django.utils.module_loading import import_string  # noqa
-except ImportError:  # Django < 1.7
+except ImportError:  # noqa
     from django.utils.module_loading import import_by_path as import_string  # noqa
