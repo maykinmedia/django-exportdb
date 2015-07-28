ad10613321410e0653e538ef253e602673f5ba76
Sergei Maertens <sergei@maykinmedia.nl>
2015-07-28 11:23:26 +0200

    Improve test setup

--- exportdb/conf.py
+++ exportdb/conf.py
@@ -6,9 +6,11 @@ from django.conf import settings
 from appconf import AppConf
 
 
+EXPORT_SUBDIR = 'exports'
+
+
 class ExportDBConf(AppConf):
-    EXPORT_SUBDIR = 'exports'
-    EXPORT_ROOT = os.path.join(settings.MEDIA_ROOT, '%s', EXPORT_SUBDIR)
+    EXPORT_ROOT = os.path.join(settings.MEDIA_ROOT, EXPORT_SUBDIR)
     EXPORT_MEDIA_URL = posixpath.join(settings.MEDIA_URL, EXPORT_SUBDIR)
     EXPORT_CONF = {}
 
